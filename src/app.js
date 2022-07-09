75298e68539c35b73f1c4d40294ca2ada0d15603
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-07-09 07:48:18 -0400

    refactor: move rotateTetromino to own file

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ import {
   setFirstTetromino,
   undrawTetromino,
   gameOver,
+  rotateTetromino,
 } from './functions/'
 
 setFirstTetromino(gameState)
@@ -30,7 +31,7 @@ function control(e) {
   if (!gameState.isGameOver && !gameState.isGamePaused) {
     // won't excute if game is over or paused
     if (e.key === 'ArrowLeft') moveLeft()
-    else if (e.key === 'ArrowUp') rotate() // ArrowUp, obviously!
+    else if (e.key === 'ArrowUp') rotateTetromino() // ArrowUp, obviously!
     else if (e.key === 'ArrowRight') moveRight()
     else if (e.key === 'ArrowDown') {
       gameState.score += 1 // softdrop score, 1pt per row
@@ -120,42 +121,6 @@ function moveRight() {
   }
 
   drawTetromino()
-}
-
-// FUNCTION FOR CORRECTING ROTATION BUG
-// prevents t,l,j,i tetrominos rotating through the edge onto the other side of the grid!!!
-function checkRotatedPosition(pos) {
-  pos = pos || gameState.currentPosition // gets current pos then checks if the piece is near the left side
-  if ((pos + 1) % WIDTH < 4) {
-    // +1 to compensate for possible difference between squares & pos
-    if (isAtRightEdge()) {
-      // check if it has rotated through to right side
-      gameState.currentPosition += 1 // if so, add 1 to push it back
-      checkRotatedPosition(pos) // check again as iTet might need +1 adjustment to pos
-    }
-  } else if (pos % WIDTH > 5) {
-    // checks if tetromino near right side
-    if (isAtLeftEdge()) {
-      // checks if it has rotated through to left side
-      gameState.currentPosition -= 1 // if so, minus 1 to push it back
-      checkRotatedPosition(pos) // check again as iTet might need +1 adjustment to pos
-    }
-  }
-}
-
-// ROTATE TETROMINO FUNCTION
-function rotate() {
-  // can cause t,l,j,i to go through wall at edge (see above)
-  undrawTetromino()
-  gameState.currentRotation++
-  if (gameState.currentRotation === gameState.currentTetromino.length) {
-    // resets rotation when gets to array end
-    gameState.currentRotation = 0
-  }
-  gameState.currentTetromino =
-    TETROMINOS[gameState.tetrominoIndex][gameState.currentRotation] // set to new rotation
-  checkRotatedPosition() // checks if tetromino has rotated through the side of grid
-  drawTetromino() // redraws tetromino
 }
 
 // START/PAUSE BTN FUNCTIONALITY
---
+++ src/functions/rotateTetromino.js
@@ -0,0 +1,37 @@
+import { gameState } from '../game-state'
+import { WIDTH, TETROMINOS } from '../constants'
+import { isAtLeftEdge, isAtRightEdge, undrawTetromino, drawTetromino } from './'
+
+// prevents t,l,j,i tetrominos rotating through the edge onto the other side of the grid!!!
+const checkRotatedPosition = (pos) => {
+  pos = pos || gameState.currentPosition // gets current pos then checks if the piece is near the left side
+  if ((pos + 1) % WIDTH < 4) {
+    // +1 to compensate for possible difference between squares & pos
+    if (isAtRightEdge()) {
+      // check if it has rotated through to right side
+      gameState.currentPosition += 1 // if so, add 1 to push it back
+      checkRotatedPosition(pos) // check again as iTet might need +1 adjustment to pos
+    }
+  } else if (pos % WIDTH > 5) {
+    // checks if tetromino near right side
+    if (isAtLeftEdge()) {
+      // checks if it has rotated through to left side
+      gameState.currentPosition -= 1 // if so, minus 1 to push it back
+      checkRotatedPosition(pos) // check again as iTet might need +1 adjustment to pos
+    }
+  }
+}
+
+export const rotateTetromino = () => {
+  // can cause t,l,j,i to go through wall at edge (see above)
+  undrawTetromino()
+  gameState.currentRotation++
+  if (gameState.currentRotation === gameState.currentTetromino.length) {
+    // resets rotation when gets to array end
+    gameState.currentRotation = 0
+  }
+  gameState.currentTetromino =
+    TETROMINOS[gameState.tetrominoIndex][gameState.currentRotation] // set to new rotation
+  checkRotatedPosition() // checks if tetromino has rotated through the side of grid
+  drawTetromino() // redraws tetromino
+}
