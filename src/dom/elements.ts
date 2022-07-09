751d36784de2d62768ef536adac65ec6e8bb51ae
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-07-09 16:29:01 -0400

    fix: update types from Element to HTMLElement

--- src/dom/elements.ts
+++ src/dom/elements.ts
@@ -1,8 +1,9 @@
-export const grid = document.querySelector('.grid') // selects specified element
-export const hiscoreDisplay = document.querySelector('#hiscore') // targets html ids
-export const scoreDisplay = document.querySelector('#score')
-export const linesDisplay = document.querySelector('#lines')
-export const levelDisplay = document.querySelector('#level')
-export const startBtn = document.querySelector('#start-button')
-export const nextTetrominoDisplay = document.querySelectorAll('.mini-grid div')
+export const grid: HTMLElement = document.querySelector('.grid')! // selects specified element
+export const hiscoreDisplay: HTMLElement = document.querySelector('#hiscore')! // targets html ids
+export const scoreDisplay: HTMLElement = document.querySelector('#score')!
+export const linesDisplay: HTMLElement = document.querySelector('#lines')!
+export const levelDisplay: HTMLElement = document.querySelector('#level')!
+export const startBtn: HTMLElement = document.querySelector('#start-button')!
+export const nextTetrominoDisplay: NodeListOf<HTMLElement> =
+  document.querySelectorAll('.mini-grid div')!
 // creates an array of the 16 divs in the mini-grid
