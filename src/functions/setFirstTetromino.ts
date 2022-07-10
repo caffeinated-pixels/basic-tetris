2ec01f6f98b16ec347193b138225e86d511b5d19
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-07-10 07:44:17 -0400

    refactor: update types

--- src/functions/setFirstTetromino.ts
+++ src/functions/setFirstTetromino.ts
@@ -1,8 +1,8 @@
-// RNG FUNCTION FOR CHOOSING A TETROMINO
+import type { GameState } from '../types'
 import { TETROMINOS } from '../constants'
 export const rng = (): number => Math.floor(Math.random() * TETROMINOS.length)
 
-export const setFirstTetromino = (gameState): void => {
+export const setFirstTetromino = (gameState: GameState): void => {
   gameState.currentTetromino =
     TETROMINOS[gameState.tetrominoIndex][gameState.currentRotation] // selects 1st rotation of the tetromino
 }
