b035e958beff09f0a7e0a05fd12f07a366fb05d9
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-07-20 07:50:25 -0400

    feat: add event listener to music button

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import { gameState } from './game-state'
 
-import { startBtn, infoBtn } from './dom/elements'
+import { startBtn, infoBtn, musicBtn } from './dom/elements'
 
 import {
   arrowKeyControls,
   preventScrolling,
   startStopGame,
   displayInfo,
+  playMusic,
 } from './functions/'
 
 document.addEventListener('keydown', arrowKeyControls)
 window.addEventListener('keydown', preventScrolling)
 startBtn.addEventListener('click', startStopGame)
 infoBtn.addEventListener('click', displayInfo)
+musicBtn.addEventListener('click', playMusic)
--- src/dom/elements.ts
+++ src/dom/elements.ts
@@ -5,6 +5,7 @@ export const linesDisplay: HTMLElement = document.querySelector('#lines')!
 export const levelDisplay: HTMLElement = document.querySelector('#level')!
 export const startBtn: HTMLElement = document.querySelector('#start-button')!
 export const infoBtn: HTMLElement = document.querySelector('#info-button')!
+export const musicBtn: HTMLElement = document.querySelector('#music-button')!
 export const gameOverMessage: HTMLElement =
   document.querySelector('#game-over')!
 export const nextTetrominoDisplay: NodeListOf<HTMLElement> =
