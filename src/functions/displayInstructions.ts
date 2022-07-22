0ba370ab87d7ff4ef9340ed2b04fbe2457f17a94
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-07-22 08:12:51 -0400

    prevent instructions appearing if game not paused

--- src/functions/displayInstructions.ts
+++ src/functions/displayInstructions.ts
@@ -2,11 +2,8 @@ import { gameState } from '../game-state'
 import { instructionsDisplay } from '../dom/elements'
 
 export const displayInstructions = (): void => {
-  console.log('displayInfo')
+  if (gameState.isGamePaused === false) return
 
-  if (gameState.isGameOver === false) return
-
-  console.log(instructionsDisplay.style.display === 'none')
   instructionsDisplay.style.display === 'none'
     ? (instructionsDisplay.style.display = 'flex')
     : (instructionsDisplay.style.display = 'none')
