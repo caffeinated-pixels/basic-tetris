506e36936cc7b7278463e08a7e399a2842688d28
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-07-23 07:50:15 -0400

    refactor: instruction display logic

--- src/functions/displayInstructions.ts
+++ src/functions/displayInstructions.ts
@@ -4,7 +4,12 @@ import { instructionsDisplay } from '../dom/elements'
 export const displayInstructions = (): void => {
   if (gameState.isGamePaused === false) return
 
-  instructionsDisplay.style.display === 'none'
-    ? (instructionsDisplay.style.display = 'flex')
-    : (instructionsDisplay.style.display = 'none')
+  if (instructionsDisplay.style.display === '') {
+    // this property will initially be an empty string
+    instructionsDisplay.style.display = 'flex'
+  } else if (instructionsDisplay.style.display === 'none') {
+    instructionsDisplay.style.display = 'flex'
+  } else {
+    instructionsDisplay.style.display = 'none'
+  }
 }
