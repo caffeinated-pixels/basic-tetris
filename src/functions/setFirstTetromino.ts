34c22596d8010097e7ea9ea15f0e83ebaa0f7cdc
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-08-20 12:46:36 -0400

    refactor:  use relative tetromino frequencies for rng

--- src/functions/setFirstTetromino.ts
+++ src/functions/setFirstTetromino.ts
@@ -1,9 +1,30 @@
-import type { GameState } from '../types'
-import { TETROMINOS } from '../constants'
+export const rng = (): number => setTetrominoIndex(Math.random())
 
-export const rng = (): number => Math.floor(Math.random() * TETROMINOS.length)
-
-export const setFirstTetromino = (gameState: GameState): void => {
-  gameState.currentTetromino =
-    TETROMINOS[gameState.tetrominoIndex][gameState.currentRotation] // selects 1st rotation of the tetromino
+const setTetrominoIndex = (random: number) => {
+  if (random < 0.107) {
+    return 0 // L
+  } else if (random < 0.245) {
+    return 1 // J
+  } else if (random < 0.382) {
+    return 2 // I
+  } else if (random < 0.519) {
+    return 3 // Z
+  } else if (random < 0.68) {
+    return 4 // O
+  } else if (random < 0.841) {
+    return 5 // S
+  } else {
+    return 6 // T
+  }
 }
+
+/* 
+The GameBoy randomizer is more complex than this, but we can
+approximate the relative tetromino frequencies:
+
+    L: 10.7 %
+    J, I, Z: 13.7 %
+    O, S, T: 16.1 % 
+
+    https://harddrop.com/wiki/Tetris_(Game_Boy)
+*/
