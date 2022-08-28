0ac940c032e3a8a0fea90fb6b3d8905d5dd69c61
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2022-08-28 08:20:09 -0400

    switch to material icons & style

--- src/functions/playMusic.ts
+++ src/functions/playMusic.ts
@@ -1,7 +1,18 @@
+import { musicBtn } from '../dom/elements'
+
 const audioUrl = new URL('../audio/robocop.mp3', import.meta.url)
 const gameMusic = new Audio(audioUrl.href)
 gameMusic.loop = true
 
+const musicBtnIcon = musicBtn.firstElementChild as HTMLSpanElement
+const musicBtnIconStrings = ['music_note', 'music_off']
+
 export const playMusic = () => {
-  gameMusic.paused === true ? gameMusic.play() : gameMusic.pause()
+  if (gameMusic.paused === true) {
+    gameMusic.play()
+    musicBtnIcon.textContent = musicBtnIconStrings[1]
+  } else {
+    gameMusic.pause()
+    musicBtnIcon.textContent = musicBtnIconStrings[0]
+  }
 }
